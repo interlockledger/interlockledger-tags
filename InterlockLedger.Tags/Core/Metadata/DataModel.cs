cf677b8d083cbebcc27634a5b5ca1d49886c6f6f
Rafael Teixeira <monoman@gmail.com>
2020-05-08 22:18:26 -0300

    Work on 4.0.4 unfinished

--- InterlockLedger.Tags/Core/Metadata/DataModel.cs
+++ InterlockLedger.Tags/Core/Metadata/DataModel.cs
@@ -164,8 +164,16 @@ namespace InterlockLedger.Tags
             return ilint;
         }
 
+        private static ILTag DeserializeItem(DataField field, object fieldValue) {
+            try {
+                return ILTag.HasDeserializer(field.TagId) ? ILTag.DeserializeFromJson(field.TagId, fieldValue) : DeserializePartialFromJson(field, fieldValue);
+            } catch (Exception e) {
+                throw new InvalidOperationException($"Could not deserialize from json field {field.Name} of type {field.TagId}\r\nfrom {fieldValue}", e);
+            }
+        }
+
         private static ILTag DeserializePartialFromJson(DataField field, object fieldValue) => field.HasSubFields
-            ? FromPartialNavigable(fieldValue as Dictionary<string, object>, field.TagId, field.SubDataFields, null)
+                    ? FromPartialNavigable(fieldValue as Dictionary<string, object>, field.TagId, field.SubDataFields, null)
             : throw new InvalidDataException($"Unknown tagId {field.TagId}");
 
         private static bool ExpandEnumeration(Dictionary<ulong, EnumerationDetails> oldEnumeration, Dictionary<ulong, EnumerationDetails> newEnumeration)
@@ -174,48 +182,54 @@ namespace InterlockLedger.Tags
         private static ILTag FromPartialNavigable(Dictionary<string, object> json, ulong tagId, IEnumerable<DataField> dataFields, DataModel dataModel) {
             if (json is null || json.Count == 0)
                 return ILTagNull.Instance;
-            const ushort minVersion = 1;
-            ushort version = minVersion;
-            var isVersioned = IsVersioned(dataFields);
-            var firstField = true;
-            var tags = new List<ILTag>();
-            foreach (var field in dataFields) {
-                if (!json.TryGetValue(field.Name, out var fieldValue))
-                    continue;
-                if (isVersioned) {
-                    if (firstField) {
-                        version = Math.Max(Convert.ToUInt16(fieldValue, CultureInfo.InvariantCulture), minVersion);
-                        firstField = false;
-                    } else if (field.Version > version)
-                        break;
-                }
-                if (fieldValue is ILTag tag) {
-                    if (tag.TagId != field.TagId)
-                        throw new InvalidCastException($"Value for field {field.Name} is a tag {tag.TagId} != {field.TagId}");
-                    tags.Add(tag);
-                } else {
-                    var item = DeserializeItem(field, fieldValue);
-                    tags.Add(item);
-                }
-            }
-            var tagsAsBytes = tags.Select(t => t.EncodedBytes).SelectMany(b => b).ToArray();
-            if (json.ContainsKey("_RemainingBytes_")) {
-                var bytesValues = json["_RemainingBytes_"];
-                var remainingBytes = bytesValues is string ? Convert.FromBase64String(bytesValues as string) : (byte[])bytesValues;
-                tagsAsBytes = tagsAsBytes.SafeConcat(remainingBytes).ToArray();
-            }
+            byte[] tagsAsBytes = JsonToBytes(json, dataFields);
             return dataModel is null ? new ILTagUnknown(tagId, tagsAsBytes) : new ILTagUnknown(dataModel, tagsAsBytes);
 
-            static ILTag DeserializeItem(DataField field, object fieldValue) {
-                try {
-                    return ILTag.HasDeserializer(field.TagId) ? ILTag.DeserializeFromJson(field.TagId, fieldValue) : DeserializePartialFromJson(field, fieldValue);
-                } catch (Exception e) {
-                    throw new InvalidOperationException($"Could not deserialize from json field {field.Name} of type {field.TagId}\r\nfrom {fieldValue}", e);
+            byte[] JsonToBytes(Dictionary<string, object> json, IEnumerable<DataField> dataFields) {
+                const ushort minVersion = 0;
+                var tags = new List<ILTag>();
+                ushort version = minVersion;
+                bool isVersioned = IsVersioned(dataFields);
+                ScanFieldsToTags(ExtractVersion());
+                return AppendRemainingBytes(tags.Select(t => t.EncodedBytes).SelectMany(b => b).ToArray());
+
+                IEnumerable<DataField> ExtractVersion() {
+                    if (!isVersioned) return dataFields;
+                    if (json.TryGetValue(dataFields.First().Name, out var fieldValue)) {
+                        version = ToUInt16(fieldValue);
+                    }
+                    tags.Add(new ILTagUInt16(version));
+                    return dataFields.Skip(1);
+                }
+
+                byte[] AppendRemainingBytes(byte[] tagsAsBytes) {
+                    if (json.ContainsKey("_RemainingBytes_")) {
+                        var bytesValues = json["_RemainingBytes_"];
+                        var remainingBytes = bytesValues is string ? Convert.FromBase64String(bytesValues as string) : (byte[])bytesValues;
+                        return tagsAsBytes.SafeConcat(remainingBytes).ToArray();
+                    }
+                    return tagsAsBytes;
+                }
+
+                void ScanFieldsToTags(IEnumerable<DataField> remainingFields) {
+                    foreach (var field in remainingFields) {
+                        if (isVersioned && field.Version > version) break;
+                        var item = json.TryGetValue(field.Name, out var fieldValue)
+                            ? fieldValue is ILTag tag ? MapItem(field, tag) : DeserializeItem(field, fieldValue)
+                            : ILTagNull.Instance;
+                        tags.Add(item);
+                    }
                 }
             }
         }
 
         private static bool IsVersioned(IEnumerable<DataField> dataFields) => dataFields?.FirstOrDefault()?.IsVersion ?? false;
+
+        private static ILTag MapItem(DataField field, ILTag tag) {
+            if (tag.TagId != field.TagId)
+                throw new InvalidCastException($"Value for field {field.Name} is a tag {tag.TagId} != {field.TagId}");
+            return tag;
+        }
 
         private static Dictionary<string, object> ToJson(Span<byte> bytes, ulong expectedTagId, IEnumerable<DataField> dataFields, ref ulong offset) {
             var json = new Dictionary<string, object>();
@@ -247,6 +261,8 @@ namespace InterlockLedger.Tags
                 json["_RemainingBytes_"] = bytes.Slice((int)offset, (int)((ulong)bytes.Length - offset)).ToArray();
             return json;
         }
+
+        private static ushort ToUInt16(object fieldValue) => Convert.ToUInt16(fieldValue, CultureInfo.InvariantCulture);
 
         private bool ExpandsOver(DataModel dm) => CompareFields(dm.DataFields, DataFields) && CompareIndexes(dm.Indexes, Indexes);
     }
