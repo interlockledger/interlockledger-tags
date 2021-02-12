3467b346b1d17b6a0a27fa9c5d6243493de68ad4
Rafael Teixeira <monoman@gmail.com>
2021-02-12 20:56:18 -0300

    v8.0.0 - Use InterlockLedger.Commons lib

--- InterlockLedger.Tags/Extensions/ObjectExtensions.cs
+++ InterlockLedger.Tags/Extensions/ObjectExtensions.cs
@@ -1,34 +1,34 @@
-/******************************************************************************************************************************
-
-Copyright (c) 2018-2020 InterlockLedger Network
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-******************************************************************************************************************************/
+// ******************************************************************************************************************************
+//  
+// Copyright (c) 2018-2021 InterlockLedger Network
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES, LOSS OF USE, DATA, OR PROFITS, OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// ******************************************************************************************************************************
 
 #nullable enable
 
@@ -55,19 +55,6 @@ namespace InterlockLedger.Tags
                 _ => IsPrimitive(value) ? value : AsILTag(ToDictionary(value))
             };
 
-        public static IEnumerable<T> AsSingle<T>(this T s) => new SingleEnumerable<T>(s);
-
-        public static List<T> AsSingleList<T>(this T s) => s.AsSingle().ToList();
-
-        public static string? PadLeft(this object? value, int totalWidth)
-            => value?.ToString()?.PadLeft(totalWidth);
-
-        public static string? PadRight(this object? value, int totalWidth)
-            => value?.ToString()?.PadRight(totalWidth);
-
-        public static T Required<T>([NotNullWhen(returnValue: true)] this T? value, string name) where T : class => value ?? throw new ArgumentException("Required", name);
-
-        public static string WithDefault(this object? value, string @default) => StringExtensions.WithDefault(value?.ToString(), @default);
 
         private static object? AsILTag(object? o)
             => o is Dictionary<string, object> dict && dict.Count == 2 && dict.ContainsKey("TagId") && dict.ContainsKey("Value")
--- InterlockLedger.Tags/Extensions/StringExtensions.cs
+++ InterlockLedger.Tags/Extensions/StringExtensions.cs
@@ -1,110 +1,46 @@
-/******************************************************************************************************************************
-
-Copyright (c) 2018-2020 InterlockLedger Network
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name of the copyright holder nor the names of its
-  contributors may be used to endorse or promote products derived from
-  this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-******************************************************************************************************************************/
+// ******************************************************************************************************************************
+//
+// Copyright (c) 2018-2021 InterlockLedger Network
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES, LOSS OF USE, DATA, OR PROFITS, OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// ******************************************************************************************************************************
 
 using System;
-using System.Linq;
-using System.Text;
 using System.Text.Json;
-using System.Text.RegularExpressions;
 
 namespace InterlockLedger.Tags
 {
     public static class StringExtensions
     {
-        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions { AllowTrailingCommas = true, WriteIndented = true };
-
-        private static readonly Regex _nameFilter = new Regex(@"[\.\s\r\n<>\:""/\\|\?\*]+");
-
-        public static ulong AsUlong(this string s) => ulong.TryParse(s?.Trim(), out var value) ? value : 0ul;
-
         public static object DeserializeJson(this string json) => string.IsNullOrWhiteSpace(json) ? null : JsonSerializer.Deserialize<object>(json, _jsonOptions).AsNavigable();
 
         public static TagHash HashOf(this string s) => TagHash.HashSha256Of(s.UTF8Bytes());
 
-        public static bool IsEmptyOrMatches(this string s, params string[] matches) => string.IsNullOrWhiteSpace(s) || matches.Any(m => s.Trim().Equals(m, StringComparison.InvariantCultureIgnoreCase));
-
-        public static string Parenthesize(this string s) => s.ParenthesizeIf(!s.Parenthesized());
-
-        public static bool Parenthesized(this string s) {
-            if (string.IsNullOrWhiteSpace(s))
-                return false;
-            s = s.Trim();
-            var count = 0;
-            if (!s.StartsWith("(", StringComparison.Ordinal)) return false;
-            for (var i = 0; i < s.Length; i++) {
-                var c = s[i];
-                if (c == '(') {
-                    count++;
-                    continue;
-                }
-                if (c == ')') {
-                    count--;
-                    if (count == 0 && (i + 1) < s.Length) return false;
-                }
-            }
-            return count == 0;
-        }
-
-        public static string ParenthesizeIf(this string s, bool condition = false) => (condition) ? $"({s})" : s;
-
-        public static string PrefixedBy(this string value, string prefix) => string.IsNullOrWhiteSpace(value) ? null : prefix + value;
-
-        public static string Reversed(this string s) => string.IsNullOrWhiteSpace(s) ? string.Empty : new string(s.ToCharArray().Reverse().ToArray());
-
-        public static string Safe(this string s) => s.WithDefault(string.Empty);
-
-        public static bool SafeEqualsTo(this string s, string other) => s is null ? other is null : s.Equals(other, StringComparison.InvariantCulture);
-
-        public static bool SafeTrimmedEqualsTo(this string s, string other) => SafeEqualsTo(s.TrimToNull(), other.TrimToNull());
-
-        public static string SimplifyAsFileName(this string name)
-            => string.IsNullOrWhiteSpace(name)
-                ? throw new ArgumentException($"Name is empty or null")
-                : _nameFilter.Replace(name.Trim(), ".").ToLowerInvariant();
-
-        public static string TrimToNull(this string s) => string.IsNullOrWhiteSpace(s) ? null : s.Trim();
-
-        public static byte[] UTF8Bytes(this string s) => Encoding.UTF8.GetBytes(s.WithDefault(string.Empty));
-
-        public static string ValidateNonEmpty(this string parameter, string parameterName)
-            => string.IsNullOrWhiteSpace(parameter) ? throw new ArgumentNullException(parameterName) : parameter.Trim();
-
-        public static string WithDefault(this string s, string @default) => string.IsNullOrWhiteSpace(s) ? @default : s.Trim();
-
-        public static string WithDefault(this string s, Func<string> resolver)
-            => resolver is null
-                ? throw new ArgumentNullException(nameof(resolver))
-                : string.IsNullOrWhiteSpace(s) ? resolver() : s.Trim();
-
-        public static string WithoutWhiteSpace(this string s) => string.IsNullOrWhiteSpace(s) ? string.Empty : Regex.Replace(s, @"[\r\n\s]+", " ").Trim();
+        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions { AllowTrailingCommas = true, WriteIndented = true };
     }
 }
